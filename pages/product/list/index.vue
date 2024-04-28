35437a0e2f7b2d58ae02209fc813c165fed942c1
DuyChinh <doanduychinh2102@gmail.com>
2024-04-28 21:37:07 +0700

    add loading search product

--- pages/product/list/index.vue
+++ pages/product/list/index.vue
@@ -1,11 +1,16 @@
+
 <script setup>
     import axios from 'axios';
     import { ref, onMounted } from "vue"
-
+    // import { VSkeletonLoader } from 'vuetify/labs/VSkeletonLoader'
     const products = ref()
     const totalProducts = ref(0)
     const infoProducts = ref()
+    const selectedPrice = ref()
+    const searchQuery = ref()
+    const priceLevels = ["100,000 - 1,000,000", "1,000,000 - 5,000,000", "5,000,000 - 20,000,00", "> 20,000,000"]
     let productData;
+    const loading = ref(false)
     const headers = [
         {
             title: 'Id',
@@ -16,7 +21,7 @@
             value: 'src'
         },
         {
-            title: 'Product name',
+            title: 'Product Name',
             value: 'title'
         },
         {
@@ -42,6 +47,7 @@
     })
 
     const getProducts = async() => {
+        loading.value = true;
         const res = await axios.get('https://apishopdc.vercel.app/products')
         .then((res) => {
             productData = res.data.data;
@@ -49,6 +55,9 @@
         })
         .catch((e) => {
             console.log(e);
+        })
+        .finally(() => {
+            loading.value = false;
         })
     }
 
@@ -67,14 +76,28 @@
     }
     watchEffect(fetchProducts)
 
+    //search product
+    const removeDiacritics = (str) => {
+        return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    };
+
+    const searchProduct = () => {
+    infoProducts.value = productData;
+    if(searchQuery.value) {
+       infoProducts.value = infoProducts.value.filter((product) => removeDiacritics(product.title.toLowerCase()).includes(removeDiacritics(searchQuery.value.toLowerCase())));
+    }
+    }
+    watchEffect(searchProduct)
+
+
     //update totalProducts
     watch(infoProducts , () => {
     if(infoProducts .value) {
-        totalProducts.value = infoProducts .value.length;
-        if(totalProducts.value <= options.value.itemsPerPage) {
-            options.value.page = 1;
+            totalProducts.value = infoProducts .value.length;
+            if(totalProducts.value <= options.value.itemsPerPage) {
+                options.value.page = 1;
+            }
         }
-    }
     });
 </script>
 
@@ -82,15 +105,101 @@
     <section class="section">
         <VCard
             title="Search Filters"
-            class="mx-6 my-6"
+            class="mx-2 my-6 border-sm"
         >
+            <VCardText>
+                <VRow>
+                    <!-- Select Price-->
+                    <VCol cols="12" sm="4">
+                        <VSelect v-model="selectedPrice"
+                            label="Select Price"
+                            :items="priceLevels"
+                            clearable
+                            clear-icon="mdi-close"
+                            variant="outlined"/>
+                    </VCol>
+                </VRow>
+            </VCardText>
+        </VCard>
+        <VCard class="mx-2 my-6 border-sm">
+            <VCardText class="d-flex justify-space-between flex-wrap gap-4">
+                <!-- Export button -->
+                <v-menu>
+                    <template v-slot:activator="{ props }">
+                        <VBtn 
+                            v-bind="props"
+                            variant="outlined"
+                            color="secondary"
+                            prepend-icon="mdi-tray-arrow-up"
+                        >
+                            Export
+                        </VBtn>
+                    </template>
+
+                    <!-- <v-list>
+                    <v-list-item class="cursor-pointer choose-file-item">
+                        <v-list-item-title>
+                            <download-excel 
+                                :data="productsInfo"
+                                name="products.xls"
+                                
+                            >
+                                Excel
+                            </download-excel>
+                        </v-list-item-title>
+                    </v-list-item>
+                    <v-list-item class="cursor-pointer choose-file-item">
+                        <v-list-item-title>
+                            <download-excel 
+                                :data="productsInfo"
+                                type="csv"
+                                name="roducts.csv"
+                            
+                                :escapeCsv="false"
+                            >
+                                CSV
+                            </download-excel>
+                        </v-list-item-title>
+                    </v-list-item>
+                    </v-list> -->
+            
+                </v-menu>
+
+                <div class="app-product-search-filter d-flex flex-wrap align-center gap-x-6 gap-y-4" >
+                    <!-- 👉 Search  -->
+                    <VTextField
+                        v-model="searchQuery"
+                        placeholder="Search Product"
+                        density="compact"
+                        style="min-inline-size: 10rem; min-width: 200px; height: 40px; margin-right: 20px; margin-top: 10px"
+                        clearable
+                        clear-icon="mdi-close"
+                        type="text"
+                        variant="outlined"
+                    />
+
+                    <!-- 👉 Add product button -->
+                    <VBtn
+                        class="order-sm-2 order-1 mt-3 text-white bg-blue"
+                        @click="isAddProduct = true"
+                    >
+                        Add Product
+                    </VBtn>
+                </div>
+  
+            </VCardText>
             <v-data-table-server class="border-sm"
-            v-model:items-per-page="options.itemsPerPage"
-            v-model:page="options.page"
-            :headers="headers"
-            :items="products"
-            :items-length="totalProducts"
+                v-model:items-per-page="options.itemsPerPage"
+                v-model:page="options.page"
+                :headers="headers"
+                :items="products"
+                :items-length="totalProducts"
+                :loading="loading"
             >
+                <!-- Loading -->
+                <template v-slot:loading>
+                    <v-skeleton-loader :type="`table-row@${ options.itemsPerPage }`"></v-skeleton-loader>
+                </template>
                 <!-- Image -->
                 <template #item.src="{ item }">
                     <img class="product-img" :src="item.src"/>
@@ -104,6 +213,11 @@
                         >
                         {{ item.title }}
                     </NuxtLink>
+                </template>
+
+                <!-- price -->
+                <template #item.price="{ item }">
+                    <span>{{ item.price.toLocaleString("en-US", {style:"currency", currency:"VND"}) }}</span>
                 </template>
 
                 <!-- Actions -->
@@ -152,7 +266,7 @@
 
 <style>
     .section {
-        padding-top: 40px;
+        /* padding-top: 40px; */
         /* background: #f7f7f9; */
     }
 
@@ -170,4 +284,21 @@
     .product-name:hover {
         opacity: 0.8;
     }
+
+    .v-field__outline {
+        border: 1px solid #ccc !important;
+        outline: none;
+        border-top-left-radius: 8px;
+        border-top-right-radius: 8px;
+    }
+
+    .v-field__loader {
+        width: 0 !important;
+    }
+
+    .v-field__overlay {
+        background: #fff !important;
+        border-radius: 6px !important;
+    }
+    
 </style>
